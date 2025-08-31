01aff668605bc2cdda38c7800e251a8a699abd9e
Mr.Orange88 <m.orange@gmx.de>
2025-08-31 11:04:36 +0200

    grundlegende HTML- und JavaScript-Dateien für das Projekt hinzugefügt

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// Variablen mit let    
+let myVariable = 27;
+
+myVariable = 32;
+// 27 wird überschrieben mit 32
+// console.log(meineVariable); // Ausgabe: 32
+console.log(myVariable);
+
+// Datentypen
+
+let meinString = "das ist ein text"; // String
+
+let myNumber = 32; // Number
+let myFloat = 32.5; // Float ist eine Zahl mit Komma
+let myInteger = 32; // Integer ist eine Zahl ohne Komma
+
+let myBoolean = true; // Boolean (true oder false)
+
+let myArray = ["Apfel", "Banane", "Orange"]; // Array (Liste)
+
+let myObject = {age : 27, name: "Alexander"}; // Object (Objekt) mit key und value
---
+++ strings_numbers.js
@@ -0,0 +1,19 @@
+
+
+let myDivison = 10/5;
+let myMultiplication = 10*5;
+
+let myPower = 2**3; // 2 hoch 3 = 8
+
+
+// Strings      
+
+let myConcat = "Hallo" + " Welt"; // Hallo Welt
+
+let myCombination = "5" + 5; // 55 (String + Number = String)
+
+console.log(myDivison);
+console.log(myMultiplication);
+console.log(myPower);
+console.log(myConcat);
+console.log(myCombination); 
